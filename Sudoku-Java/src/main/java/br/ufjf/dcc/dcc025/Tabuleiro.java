bcf63049dc78ccd17ca4b6d3eea2b058472823f7
RomuloAmaral <romuloamaral32@gmail.com>
2025-01-03 23:27:21 -0300

    faz a classe tabuleiro

---
+++ Sudoku-Java/src/main/java/br/ufjf/dcc/dcc025/Tabuleiro.java
@@ -0,0 +1,69 @@
+package br.ufjf.dcc.dcc025;
+
+public class Tabuleiro {
+    private Elemento[][] grid; // Representa o tabuleiro do Sudoku com Elementos
+
+    // Construtor
+    public Tabuleiro() {
+        grid = new Elemento[9][9]; // Inicializa um tabuleiro 9x9
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                grid[i][j] = new Elemento(i, j, 0); // Inicializa cada elemento com valor 0
+            }
+        }
+    }
+
+    // Método para definir um valor no tabuleiro
+    public void setValor(int linha, int coluna, int valor) {
+        Elemento elemento = grid[linha][coluna];
+        if (elemento.getPossibleValues().contains(valor)) {
+            elemento.setValor(valor);
+            atualizarPossiveis(linha, coluna, valor); // Remove o valor das possíveis posições da linha, coluna e quadrante
+        } else {
+            System.out.println("Valor inválido para essa posição.");
+        }
+    }
+
+    // Método para obter um valor do tabuleiro
+    public int getValor(int linha, int coluna) {
+        return grid[linha][coluna].getValor();
+    }
+
+    // Método para imprimir o tabuleiro
+    public void imprimirTabuleiro() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(grid[i][j].getValor() + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Método que atualiza as listas de valores possíveis na linha, coluna e quadrante 3x3
+    private void atualizarPossiveis(int linha, int coluna, int valor) {
+        // Remover valor das possíveis posições na linha
+        for (int i = 0; i < 9; i++) {
+            if (i != coluna) {
+                grid[linha][i].removePossibleValue(valor);
+            }
+        }
+
+        // Remover valor das possíveis posições na coluna
+        for (int i = 0; i < 9; i++) {
+            if (i != linha) {
+                grid[i][coluna].removePossibleValue(valor);
+            }
+        }
+
+        // Remover valor das possíveis posições no quadrante 3x3
+        int quadInicioLinha = (linha / 3) * 3;
+        int quadInicioColuna = (coluna / 3) * 3;
+        for (int i = quadInicioLinha; i < quadInicioLinha + 3; i++) {
+            for (int j = quadInicioColuna; j < quadInicioColuna + 3; j++) {
+                if (i != linha || j != coluna) {
+                    grid[i][j].removePossibleValue(valor);
+                }
+            }
+        }
+    }
+}
